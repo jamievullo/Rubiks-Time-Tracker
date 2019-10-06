22e84c3a380add70cc1f39875e59381894c04f5f
jamievullo <jvullo@protonmail.com>
2019-10-06 11:52:10 -0400

    Added routes and basic controller actions

--- app/controllers/cube_times_controller.rb
+++ app/controllers/cube_times_controller.rb
@@ -1,2 +1,30 @@
 class CubeTimesController < ApplicationController
+
+    def show 
+    end
+
+    def index 
+    end
+
+    def new 
+    end
+
+    def create 
+    end
+
+    def edit 
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def cube_times_params
+
+    end
+    
 end
--- app/controllers/cubers_controller.rb
+++ app/controllers/cubers_controller.rb
@@ -1,2 +1,17 @@
 class CubersController < ApplicationController
+    
+    def index 
+
+    end
+
+    def show 
+
+    end
+
+    private
+
+    def cubers_params
+
+    end
+
 end
